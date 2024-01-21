d008243dae461cd86de7d8345e8cd81f8982fe75
Bytewright <bytewright@t-online.de>
2024-01-21 11:01:05 +0100

    initial mvn java spring boot

---
+++ core/src/test/java/de/bytewright/dailylog/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bytewright.dailylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
